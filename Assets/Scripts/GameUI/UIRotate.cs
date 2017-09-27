38d0275a2885aa537c5816c7f89ecd6aebcecb32
unknown <>
2017-09-27 23:35:56 +0800

    Now Login Button Can Rotate Itself

---
+++ Assets/Scripts/GameUI/UIRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRotate : MonoBehaviour {
+    private ICanvasElement rotateTarget;
+
+    public UIRotate(ICanvasElement rotateTarget) {
+        this.rotateTarget = rotateTarget;
+    }
+    
+    public void Rotate(float rotateAngle,float rotationTime) {
+        rotateTarget.transform.Rotate(new Vector3(0,0,rotateAngle));
+    }
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
