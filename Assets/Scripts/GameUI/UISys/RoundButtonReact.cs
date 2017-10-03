a04708f251e795a8dda14d35a67ac551e8d53ea3
unknown <>
2017-10-03 20:41:15 +0800

    hi

---
+++ Assets/Scripts/GameUI/UISys/RoundButtonReact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RoundButtons {
+    public class RoundButtonReact : MonoBehaviour {
+
+        public virtual void React() {
+
+        }
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
+
--- Assets/Scripts/GameUI/UISys/RoundButtonSet.cs
+++ Assets/Scripts/GameUI/UISys/RoundButtonSet.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Experimental.UIElements;
 using Image = UnityEngine.UI.Image;
@@ -9,95 +10,119 @@ using Image = UnityEngine.UI.Image;
 /// 圆形按钮集合
 /// 
 /// </summary>
-public class RoundButtonSet : MonoBehaviour {
-    public List<string> TextList;
-    public Sprite sp;
-    public Canvas ca;
-    public float perc = 0.8f;
-    private float litr = 10;
-    private float bigr = 50;
-    private int countNum = 0;
-    private float leftSpace = 0f;
-    private List<GameObject> circleGameObjects = new List<GameObject>();
+/// 
 
-    void Start () {
-        countNum = TextList.Count;
-        float eachSize = 1.0f / countNum * perc;
-        leftSpace = 1.0f / countNum - eachSize;
-        float shift = 0;
-        var rectTrans = this.gameObject.GetComponent<RectTransform>();
-        bigr = rectTrans.rect.width * rectTrans.localScale.x * 0.5f;
-        litr = rectTrans.rect.width * rectTrans.localScale.x * 0.3f;
-
-
-        GameObject go = null;
-        foreach (string textInfo in TextList) {
-            go  =  new GameObject( textInfo );
-
-            go.AddComponent<Image>();
-            go.GetComponent<Image>().sprite = sp;
-            go.GetComponent<Image>().type  = Image.Type.Filled;
-            go.GetComponent<Image>().fillAmount = eachSize;
-            go.GetComponent<Image>().fillOrigin = (int) Image.Origin360.Top;
-            go.GetComponent<Image>().color = new Color((shift+1)*0.2f, (shift + 1) * 0.2f, (shift + 1) * 0.2f );
-
-
-            go.transform.SetParent(this.gameObject.transform);
-
-
-            go.GetComponent<RectTransform>().anchoredPosition = new Vector2( 0, 0 );
-            go.GetComponent<RectTransform>().localScale =  new Vector3(1f,1f,1f);
-            go.GetComponent<RectTransform>().Rotate( 0, 0,  360  *　shift / countNum - 360 *leftSpace*0.5f );
-       //      go.AddComponent<ButtonAction>();
-
-            go.layer = LayerMask.NameToLayer("UI");
-            
-
-            shift += 1.0f;
-            circleGameObjects.Add(go);
-        }
-  
-
+namespace RoundButtons {
+    [System.Serializable]
+    public class ButtonData {
+        public string name = "输入名字";
+        public MonoScript mono;
     }
+    public class RoundButtonSet : MonoBehaviour {
+        public List<ButtonData> DataList;
+        public Sprite sp;
+        public Canvas ca;
+        public float perc = 0.8f;
+        private float litr = 10;
+        private float bigr = 50;
+        private int countNum = 0;
+        private float leftSpace = 0f;
+        private List<GameObject> circleGameObjects = new List<GameObject>();
 
-    public void ResetAllButton() {
-        foreach(var gmo in circleGameObjects) {
-            gmo.GetComponent<RectTransform>().localScale = new Vector3( 1, 1, 1 );
-        }
-        var rectTrans = this.gameObject.GetComponent<RectTransform>();
-        bigr = rectTrans.rect.width * rectTrans.localScale.x * 0.5f;
-    }
-	void Update () {
-	    Vector2 m2 = new Vector2(0,0);
- 
-        var  rectTransform = ca.transform as RectTransform;
-        //获取当前屏幕的鼠标坐标
-       
-	    RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, Input.mousePosition, ca.worldCamera, out m2);
-	   // Debug.Log(m2);
-
-        //判断是否在圆上
-	    if (m2.magnitude > litr && m2.magnitude < bigr) {
-	        float res = Mathf.Atan2(m2.y, m2.x) * Mathf.Rad2Deg;
-	        res = res > 90 ? res : res + 360;
-	        res = res - 90;
-	        int resTarget = (int) (res / (360 / countNum));
-	        DebugConsole.Log(res.ToString() + "," + (res - resTarget * 360 / countNum).ToString());
+        void Start() {
+            countNum = DataList.Count;
+            float eachSize = 1.0f / countNum * perc;
+            leftSpace = 1.0f / countNum - eachSize;
+            float shift = 0;
+            var rectTrans = this.gameObject.GetComponent<RectTransform>();
+            bigr = rectTrans.rect.width * rectTrans.localScale.x * 0.5f;
+            litr = rectTrans.rect.width * rectTrans.localScale.x * 0.3f;
 
 
-	        if (res - resTarget * 360 / countNum < leftSpace * 180 ||
-	            res - resTarget * 360.0f / countNum > 360.0f / countNum - leftSpace * 180) {
-                 ResetAllButton();
-	        }
-	        else {
-	            circleGameObjects[(resTarget+1)%countNum].GetComponent<RectTransform>().localScale = new Vector3(1.05f, 1.05f, 1.05f);
-                var rectTrans = this.gameObject.GetComponent<RectTransform>();
-                bigr  = rectTrans.rect.width * rectTrans.localScale.x * 0.5f * 1.05f;
+            GameObject go = null;
+            List<string> lis = new List<string>();
+            foreach(var buttonData in DataList) {
+                go = new GameObject( buttonData.name );
+
+                go.AddComponent<Image>();
+                go.GetComponent<Image>().sprite = sp;
+                go.GetComponent<Image>().type = Image.Type.Filled;
+                go.GetComponent<Image>().fillAmount = eachSize;
+                go.GetComponent<Image>().fillOrigin = (int)Image.Origin360.Top;
+                go.GetComponent<Image>().color = new Color( (shift + 1) * 0.2f, (shift + 1) * 0.2f, (shift + 1) * 0.2f );
+
+
+                go.transform.SetParent( this.gameObject.transform );
+
+
+                go.GetComponent<RectTransform>().anchoredPosition = new Vector2( 0, 0 );
+                go.GetComponent<RectTransform>().localScale = new Vector3( 1f, 1f, 1f );
+                go.GetComponent<RectTransform>().Rotate( 0, 0, 360 * shift / countNum - 360 * leftSpace * 0.5f );
+                //      go.AddComponent<ButtonAction>();
+
+                go.layer = LayerMask.NameToLayer( "UI" );
+                Type t = buttonData.mono.GetClass();
+                if(t.IsAssignableFrom( typeof( RoundButtonReact ) )) {
+                    go.AddComponent( t );
+                }
+                else {
+                    Debug.LogError( "Script should be a subclass of RoundButtonReact" );
+                }
+
+
+                shift += 1.0f;
+                circleGameObjects.Add( go );
             }
 
-	    }
-	    else { 
-	        ResetAllButton();
-	    }
-	}
+
+        }
+
+        public void ResetAllButton() {
+            foreach(var gmo in circleGameObjects) {
+                gmo.GetComponent<RectTransform>().localScale = new Vector3( 1, 1, 1 );
+            }
+            var rectTrans = this.gameObject.GetComponent<RectTransform>();
+            bigr = rectTrans.rect.width * rectTrans.localScale.x * 0.5f;
+        }
+        void Update() {
+            Vector2 m2 = new Vector2( 0, 0 );
+
+            var rectTransform = ca.transform as RectTransform;
+            //获取当前屏幕的鼠标坐标
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle( rectTransform, Input.mousePosition, ca.worldCamera, out m2 );
+            // Debug.Log(m2);
+
+            //判断是否在圆上
+            if(m2.magnitude > litr && m2.magnitude < bigr) {
+                float res = Mathf.Atan2( m2.y, m2.x ) * Mathf.Rad2Deg;
+                res = res > 90 ? res : res + 360;
+                res = res - 90;
+                int resTarget = (int)(res / (360 / countNum));
+                DebugConsole.Log( res.ToString() + "," + (res - resTarget * 360 / countNum).ToString() );
+
+
+                if(res - resTarget * 360 / countNum < leftSpace * 180 ||
+                    res - resTarget * 360.0f / countNum > 360.0f / countNum - leftSpace * 180) {
+                    ResetAllButton();
+                }
+                else {
+                    circleGameObjects[(resTarget + 1) % countNum].GetComponent<RectTransform>().localScale = new Vector3( 1.05f, 1.05f, 1.05f );
+                    var rectTrans = this.gameObject.GetComponent<RectTransform>();
+                    bigr = rectTrans.rect.width * rectTrans.localScale.x * 0.5f * 1.05f;
+                    if(Input.GetMouseButtonDown( 0 )) {
+                        Type t = DataList[(resTarget + 1) % countNum].mono.GetClass();
+                        var q = circleGameObjects[(resTarget + 1) % countNum].GetComponent(t) as RoundButtonReact;
+                        if (q != null) q.React();
+                    }
+                }
+
+            }
+            else {
+                ResetAllButton();
+            }
+        }
+    }
+
+
 }
