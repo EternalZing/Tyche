faaaba524aa0d4d5f126ecc8eab077451ea9797d
taoria <445625470@qq.com>
2017-10-09 21:00:18 +0800

    complete the login button

--- Assets/Scripts/GameUI/UISys/RoundButtonSet.cs
+++ Assets/Scripts/GameUI/UISys/RoundButtonSet.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
 using Image = UnityEngine.UI.Image;
 
 /// <summary>
@@ -54,9 +55,10 @@ namespace RoundButtons {
 
                 go.transform.SetParent( this.gameObject.transform );
 
-
+                go.transform.position = new Vector3(0,0,0);
                 go.GetComponent<RectTransform>().anchoredPosition = new Vector2( 0, 0 );
                 go.GetComponent<RectTransform>().localScale = new Vector3( 1f, 1f, 1f );
+                go.GetComponent<RectTransform>().sizeDelta = new Vector2(500,500);
                 go.GetComponent<RectTransform>().Rotate( 0, 0, 360 * shift / countNum - 360 * leftSpace * 0.5f );
                 //      go.AddComponent<ButtonAction>();
 
@@ -69,6 +71,29 @@ namespace RoundButtons {
                     Debug.LogError( "Script should be a subclass of RoundButtonReact" );
                 }
 
+
+                int length = buttonData.name.Length;
+                GameObject tx = null;
+                float startDir = 360 * leftSpace*0.5f;
+                float dis = (bigr + litr) * 0.55f;
+                foreach (char c in buttonData.name) {
+                    tx = new GameObject( c.ToString());
+                    tx.AddComponent<Text>();
+                    tx.GetComponent<Text>().text = c.ToString();
+                    tx.layer = LayerMask.NameToLayer( "UI" );
+                    tx.transform.SetParent(go.transform);
+                    tx.GetComponent<RectTransform>()
+                        .anchoredPosition =  new Vector2(dis * Mathf.Sin(  startDir *Mathf.Deg2Rad),
+                            dis * Mathf.Cos(  startDir*Mathf.Deg2Rad ) );
+                    tx.GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
+                    Debug.Log( c + "," + startDir );
+                    startDir += (360.0f * eachSize *0.75f) / (float)length;
+                    tx.GetComponent<Text>().font = Font.CreateDynamicFontFromOSFont("黑体",45);
+                    tx.GetComponent<Text>().fontSize = 40;
+                    tx.GetComponent<Text>().color = new Color(0.7f,0,0);
+                    tx.GetComponent<RectTransform>().sizeDelta = new Vector2(45,45);
+                    tx.GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
+                }
 
                 shift += 1.0f;
                 circleGameObjects.Add( go );
@@ -99,7 +124,7 @@ namespace RoundButtons {
                 res = res > 90 ? res : res + 360;
                 res = res - 90;
                 int resTarget = (int)(res / (360 / countNum));
-                DebugConsole.Log( res.ToString() + "," + (res - resTarget * 360 / countNum).ToString() );
+             
 
 
                 if(res - resTarget * 360 / countNum < leftSpace * 180 ||
